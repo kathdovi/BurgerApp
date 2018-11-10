cbfa46a3d287c07baa75234191ae64a04ff288b4
Kathleen Doviken <kathleendoviken@gmail.com>
2018-11-10 07:50:03 -0600

    project

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Avocado Black Bean Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Impossible burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Portobello Mushroom Burger');
